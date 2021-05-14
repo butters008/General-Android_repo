a837fec0cf57a8fbf027a314202efdab9d1e07b8
Butters <k.butterfield08@gmail.com>
2021-05-14 07:19:19 -0500

    Got some more done
    Got some more lessons done! Working at it slowly but surely!

---
+++ Section 5/Show and Hide UI/app/src/main/java/com/example/section5_show_hide_ui/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.section5_show_hide_ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView textView;
+
+
+    public void show(View view){
+        textView.setVisibility(View.VISIBLE);
+    }
+
+    public void hide(View view){
+        textView.setVisibility(View.INVISIBLE);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+}
