5b6db8edf6d191331feefed95e857e8512e0a8ab
Butters <k.butterfield08@gmail.com>
2021-07-13 18:42:09 -0500

    updated
    updated

---
+++ Other Android Course/CardView App/app/src/main/java/com/example/cardviewapp/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.cardviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.mainActButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, ScrollingActivity.class));
+            }
+        });
+    }
+}
